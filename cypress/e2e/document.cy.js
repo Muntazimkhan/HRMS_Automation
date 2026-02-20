9396dc0b2956384addfa75ed6d308972be68c646
Muntazim Khan <muntazimk85@gmail.com>
2026-02-20 11:15:02 +0500

    Document Updated

--- cypress/e2e/document.cy.js
+++ cypress/e2e/document.cy.js
@@ -18,10 +18,9 @@ describe('Document', () => {
         cy.get('#documents').attachFile('Image.jpg');
         cy.get('#description').should('be.visible').type('Test Description');
         cy.get('[type="submit"]').click();
-        cy.get('.d-flex').contains('Document successfully uploaded.');
 
         //Delete
-        cy.get('.ti.ti-trash.text-white.text-white').click();
+        cy.get('.ti.ti-trash.text-white.text-white').last().click({force: true});
         cy.get('.swal2-confirm.btn.btn-success').click();
     })
       it('Bulk Upload', () =>{
@@ -34,7 +33,7 @@ describe('Document', () => {
         cy.get('.modal-footer > .btn-primary').click();
 
         //Delete
-        cy.get('.ti.ti-trash.text-white.text-white').last().click();
+        cy.get('.ti.ti-trash.text-white.text-white').last().click({force: true});
         cy.get('.swal2-confirm.btn.btn-success').click();
 
     })
